842b624c46b0ca32700e4d770c9ba06bce43eddb
bitdomo <toth.dominik92@gmail.com>
2015-04-23 00:43:29 +0200

    Update gapps

--- hammerhead-vendor-blobs.mk
+++ hammerhead-vendor-blobs.mk
@@ -261,8 +261,9 @@ PRODUCT_COPY_FILES += \
 	vendor/lge/hammerhead/proprietary/app/GoogleCamera/lib/arm/librsjni.so:system/app/GoogleCamera/lib/arm/librsjni.so \
 	vendor/lge/hammerhead/proprietary/app/GoogleCamera/lib/arm/libRSSupport.so:system/app/GoogleCamera/lib/arm/libRSSupport.so \
 	vendor/lge/hammerhead/proprietary/app/Velvet/Velvet.apk:system/priv-app/Velvet/Velvet.apk \
-	vendor/lge/hammerhead/proprietary/app/Velvet/lib/arm/libgoogle_hotword_jni.so:system/priv-app/Velvet/lib/arm/libgoogle_hotword_jni.so \
-	vendor/lge/hammerhead/proprietary/app/Velvet/lib/arm/libgoogle_recognizer_jni_l.so:system/priv-app/Velvet/lib/arm/libgoogle_recognizer_jni_l.so \
+	vendor/lge/hammerhead/proprietary/app/Velvet/lib/arm/libcronet.so:system/priv-app/Velvet/lib/arm/libcronet.so \
+	vendor/lge/hammerhead/proprietary/app/Velvet/lib/arm/libgoogle_speech_jni.so:system/priv-app/Velvet/lib/arm/libgoogle_speech_jni.so \
+	vendor/lge/hammerhead/proprietary/app/Velvet/lib/arm/libgoogle_speech_micro_jni.so:system/priv-app/Velvet/lib/arm/libgoogle_speech_micro_jni.so \
 	vendor/lge/hammerhead/proprietary/app/Velvet/lib/arm/libvcdecoder_jni.so:system/priv-app/Velvet/lib/arm/libvcdecoder_jni.so \
 	vendor/lge/hammerhead/proprietary/app/GoogleHome/GoogleHome.apk:system/app/GoogleHome/GoogleHome.apk \
 	vendor/lge/hammerhead/proprietary/app/FaceLock/FaceLock.apk:system/app/FaceLock/FaceLock.apk \
@@ -277,5 +278,38 @@ PRODUCT_COPY_FILES += \
 	vendor/lge/hammerhead/proprietary/vendor/pittpatt/models/detection/yaw_roll_face_detectors.7.1/head-y0-yi45-p0-pi45-rp30-ri30.5-v24-tree7-2-wmd.bin:system/vendor/pittpatt/models/detection/yaw_roll_face_detectors.7.1/head-y0-yi45-p0-pi45-rp30-ri30.5-v24-tree7-2-wmd.bin \
 	vendor/lge/hammerhead/proprietary/vendor/pittpatt/models/detection/yaw_roll_face_detectors.7.1/pose-r.8.1.bin:system/vendor/pittpatt/models/detection/yaw_roll_face_detectors.7.1/pose-r.8.1.bin \
 	vendor/lge/hammerhead/proprietary/vendor/pittpatt/models/detection/yaw_roll_face_detectors.7.1/pose-y-r.8.1.bin:system/vendor/pittpatt/models/detection/yaw_roll_face_detectors.7.1/pose-y-r.8.1.bin \
-	vendor/lge/hammerhead/proprietary/vendor/pittpatt/models/recognition/face.face.y0-y0-71-N-tree_7-wmd.bin:system/vendor/pittpatt/models/recognition/face.face.y0-y0-71-N-tree_7-wmd.bin
-	
+	vendor/lge/hammerhead/proprietary/vendor/pittpatt/models/recognition/face.face.y0-y0-71-N-tree_7-wmd.bin:system/vendor/pittpatt/models/recognition/face.face.y0-y0-71-N-tree_7-wmd.bin \
+	vendor/lge/hammerhead/proprietary/app/GoogleBackupTransport/GoogleBackupTransport.apk:system/priv-app/GoogleBackupTransport/GoogleBackupTransport.apk \
+	vendor/lge/hammerhead/proprietary/app/GoogleContactsSyncAdapter/GoogleContactsSyncAdapter.apk:system/app/GoogleContactsSyncAdapter/GoogleContactsSyncAdapter.apk \
+	vendor/lge/hammerhead/proprietary/app/GoogleFeedback/GoogleFeedback.apk:system/priv-app/GoogleFeedback/GoogleFeedback.apk \
+	vendor/lge/hammerhead/proprietary/app/GoogleLoginService/GoogleLoginService.apk:system/priv-app/GoogleLoginService/GoogleLoginService.apk \
+	vendor/lge/hammerhead/proprietary/app/GoogleOneTimeInitializer/GoogleOneTimeInitializer.apk:system/priv-app/GoogleOneTimeInitializer/GoogleOneTimeInitializer.apk \
+	vendor/lge/hammerhead/proprietary/app/GooglePartnerSetup/GooglePartnerSetup.apk:system/priv-app/GooglePartnerSetup/GooglePartnerSetup.apk \
+	vendor/lge/hammerhead/proprietary/app/GoogleServicesFramework/GoogleServicesFramework.apk:system/priv-app/GoogleServicesFramework/GoogleServicesFramework.apk \
+	vendor/lge/hammerhead/proprietary/app/Phonesky/Phonesky.apk:system/priv-app/Phonesky/Phonesky.apk \
+	vendor/lge/hammerhead/proprietary/app/PrebuiltGmsCore/PrebuiltGmsCore.apk:system/priv-app/PrebuiltGmsCore/PrebuiltGmsCore.apk \
+	vendor/lge/hammerhead/proprietary/app/PrebuiltGmsCore/lib/arm/libAppDataSearch.so:system/priv-app/PrebuiltGmsCore/lib/arm/libAppDataSearch.so \
+	vendor/lge/hammerhead/proprietary/app/PrebuiltGmsCore/lib/arm/libconscrypt_gmscore_jni.so:system/priv-app/PrebuiltGmsCore/lib/arm/libconscrypt_gmscore_jni.so \
+	vendor/lge/hammerhead/proprietary/app/PrebuiltGmsCore/lib/arm/libgames_rtmp_jni.so:system/priv-app/PrebuiltGmsCore/lib/arm/libgames_rtmp_jni.so \
+	vendor/lge/hammerhead/proprietary/app/PrebuiltGmsCore/lib/arm/libgcastv2_base.so:system/priv-app/PrebuiltGmsCore/lib/arm/libgcastv2_base.so \
+	vendor/lge/hammerhead/proprietary/app/PrebuiltGmsCore/lib/arm/libgcastv2_support.so:system/priv-app/PrebuiltGmsCore/lib/arm/libgcastv2_support.so \
+	vendor/lge/hammerhead/proprietary/app/PrebuiltGmsCore/lib/arm/libgmscore.so:system/priv-app/PrebuiltGmsCore/lib/arm/libgmscore.so \
+	vendor/lge/hammerhead/proprietary/app/PrebuiltGmsCore/lib/arm/libgms-ocrclient.so:system/priv-app/PrebuiltGmsCore/lib/arm/libgms-ocrclient.so \
+	vendor/lge/hammerhead/proprietary/app/PrebuiltGmsCore/lib/arm/libjgcastservice.so:system/priv-app/PrebuiltGmsCore/lib/arm/libjgcastservice.so \
+	vendor/lge/hammerhead/proprietary/app/PrebuiltGmsCore/lib/arm/libNearbyApp.so:system/priv-app/PrebuiltGmsCore/lib/arm/libNearbyApp.so \
+	vendor/lge/hammerhead/proprietary/app/PrebuiltGmsCore/lib/arm/libsslwrapper_jni.so:system/priv-app/PrebuiltGmsCore/lib/arm/libsslwrapper_jni.so \
+	vendor/lge/hammerhead/proprietary/app/PrebuiltGmsCore/lib/arm/libwearable-selector.so:system/priv-app/PrebuiltGmsCore/lib/arm/libwearable-selector.so \
+	vendor/lge/hammerhead/proprietary/app/PrebuiltGmsCore/lib/arm/libWhisper.so:system/priv-app/PrebuiltGmsCore/lib/arm/libWhisper.so \
+	vendor/lge/hammerhead/proprietary/app/SetupWizard/SetupWizard.apk:system/priv-app/SetupWizard/SetupWizard.apk \
+	vendor/lge/hammerhead/proprietary/etc/permissions/com.google.android.camera2.xml:system/etc/permissions/com.google.android.camera2.xml \
+	vendor/lge/hammerhead/proprietary/etc/permissions/com.google.android.maps.xml:system/etc/permissions/com.google.android.maps.xml \
+	vendor/lge/hammerhead/proprietary/etc/permissions/com.google.android.media.effects.xml:system/etc/permissions/com.google.android.media.effects.xml \
+	vendor/lge/hammerhead/proprietary/etc/permissions/com.google.widevine.software.drm.xml:system/etc/permissions/com.google.widevine.software.drm.xml \
+	vendor/lge/hammerhead/proprietary/etc/preferred-apps/google.xml:system/etc/preferred-apps/google.xml \
+	vendor/lge/hammerhead/proprietary/framework/com.google.widevine.software.drm.jar:system/framework/com.google.widevine.software.drm.jar \
+	vendor/lge/hammerhead/proprietary/framework/com.google.android.media.effects.jar:system/framework/com.google.android.media.effects.jar \
+	vendor/lge/hammerhead/proprietary/framework/com.google.android.maps.jar:system/framework/com.google.android.maps.jar \
+	vendor/lge/hammerhead/proprietary/framework/com.google.android.camera2.jar:system/framework/com.google.android.camera2.jar \
+	vendor/lge/hammerhead/proprietary/lib/libjni_latinime.so:system/lib/libjni_latinime.so \
+	vendor/lge/hammerhead/proprietary/app/LatinImeGoogle/LatinImeGoogle.apk:system/app/LatinImeGoogle/LatinImeGoogle.apk \
+	vendor/lge/hammerhead/proprietary/app/LatinImeGoogle/lib/arm/libjni_unbundled_latinimegoogle.so:system/app/LatinImeGoogle/lib/arm/libjni_unbundled_latinimegoogle.so
